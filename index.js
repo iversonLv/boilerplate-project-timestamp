8a3a1122e783169ee91df8db01c0573ba33b4e85
RUMI LU <iverson_3_3_3@126.com>
2025-05-25 17:25:58 +0000

    updated

--- index.js
+++ index.js
@@ -20,10 +20,23 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  const {date} = req.params
+  let result;
+  // check the date params is empty or not
+  if(!date) {
+    result = new Date()
+    return res.json({unix: result.getTime(), utc: result.toUTCString()});
+  }
+  // check the non-empty date params is string or 
+  result = isNaN(+date) ? new Date(date) : new Date(+date) 
+  if(isNaN(result)) {
+    return res.json({ error : "Invalid Date" })
+  }
+  // check the date is valida date or not
+  res.json({unix: result.getTime(), utc: result.toUTCString()});
+  
 });
-
 
 
 // Listen on port set in environment variable or default to 3000
